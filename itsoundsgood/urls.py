ce197710a7739fef10ef236381f3e1625a0f96a2
Harry Yuan <harryuan.65@gmail.com>
2019-08-16 11:57:39 +0800

    set homepage to it sounds good

--- itsoundsgood/urls.py
+++ itsoundsgood/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('program/',include('program.urls'))
+    path('',include('program.urls'))
+    # path('program/',include('program.urls'))
 ]
 
 urlpatterns+= staticfiles_urlpatterns()
--- program/urls.py
+++ program/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home ,name='index'),
-  path('render_index', views.render_index,name='render_index'),
+  # path('', views.home ,name='index'),
+  path('', views.render_index,name='render_index'),
   # path('upload_file', views.upload_file,name='upload_file'),
   path('send_json', views.send_json ,name='send_json'),
 ]
