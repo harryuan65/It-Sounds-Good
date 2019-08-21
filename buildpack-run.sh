3e4f2a024378d373c022d3e44b7048f5b4ec63b4
Harry Yuan <harryuan.65@gmail.com>
2019-08-21 18:23:58 +0800

    Purge cache

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,8 +1,9 @@
 # echo 'Please leave me alone, soundfile.'
 # pip uninstall Soundfile
-tar xzvf libsndfile-1.0.28.tar.gz
+# tar xzvf libsndfile-1.0.28.tar.gz
 
-cd libsndfile-1.0.28
-./configure
-make -s
-make install
+# cd libsndfile-1.0.28
+# ./configure
+# make -s
+# make install
+echo 'Fuck this shit doesnt allow me to install libsndfile'
