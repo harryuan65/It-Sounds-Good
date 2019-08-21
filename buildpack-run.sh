bf911e371f15ed90c32bcce0ab71b26f89c0774c
Harry Yuan <harryuan.65@gmail.com>
2019-08-21 17:59:07 +0800

    wrong filename

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,8 +1,8 @@
 # echo 'Please leave me alone, soundfile.'
 # pip uninstall Soundfile
-tar xzvf libsndfile-1.0.27.tar.gz
+tar xzvf libsndfile-1.0.28.tar.gz
 
-cd libsndfile-1.0.27
+cd libsndfile-1.0.28
 ./configure
 make -s
 make install
