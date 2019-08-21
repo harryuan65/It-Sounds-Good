0eb473edb6764d31eac596d702135a2ac2a458f5
Harry Yuan <harryuan.65@gmail.com>
2019-08-21 17:52:48 +0800

    tried to use cli to install libsndfile

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,8 @@
-echo 'Please leave me alone, soundfile.'
-pip uninstall Soundfile
+# echo 'Please leave me alone, soundfile.'
+# pip uninstall Soundfile
+tar xzvf libsndfile-1.0.27.tar.gz
+
+cd libsndfile-1.0.27
+./configure
+make -s
+make install
